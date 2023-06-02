e5764b620012e9df10094ae1145dee8c0b8cb8b1
Jole24 <jolerep@gmail.com>
2023-06-02 10:04:16 -0500

    update post.jsx

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,16 +1,25 @@
 import { Link } from "@remix-run/react"
 import { formatDate } from "../utils/helpers"
+import { useEffect, useState } from "react"
 
 export default function Post({ posts }) {
 
     const {uuid, title, content, image, publishedAt } = posts
+
+    const [date, setDate] = useState(null)
+
+    useEffect(() => {
+
+        setDate(formatDate(publishedAt))
+    
+    }, [])
 
     return (
         <article className="post">
             <img src={image} className="imagen" alt={`Imagen del post ${title}`}/>
             <div className="contenido">
                 <h1>{title}</h1>
-                <p className="fecha">Publicado el {formatDate(publishedAt)}</p>
+                <p className="fecha">Publicado el {date}</p>
                 <p className="resumen">{content}</p>
                 <Link className="enlace" to={`/posts/${uuid}`}>Leer mas</Link>
             </div>
--- app/routes/posts.$postUrl.jsx
+++ app/routes/posts.$postUrl.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "@remix-run/react"
 import { getSinglePost } from "../models/posts.server"
 import { formatDate } from "../utils/helpers"
 import styles from '~/styles/blog.css'
+import { useEffect, useState } from "react"
 
 export function meta({data}){
 
@@ -53,12 +54,22 @@ export default function Post() {
 
     const posts = useLoaderData()
     const { title, content, image, publishedAt } = posts[0]
+    const [date, setDate] = useState(null)
+
+    useEffect(() => {
+
+        setDate(formatDate(publishedAt))
+    
+    }, [])
+    
+
+
     return (
         <article className="contenedor post-single">
             <img src={image} className="imagen-post" alt={`Imagen del post ${title}`}/>
             <div className="contenido">
                 <h3>{title}</h3>
-                <p className="fecha">Publicado el {formatDate(publishedAt)}</p>
+                <p className="fecha">Publicado el {date}</p>
                 <ul>
                     {
                         content.split('\n').map((item, index) => (
